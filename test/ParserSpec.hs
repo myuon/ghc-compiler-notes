1d53382b1b31313ff6cd71f0834f4f53ecdc6b41
myuon <ioi.joi.koi.loi@gmail.com>
2018-09-18 01:36:13 +0900

    feat: add parseDoc

--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -7,7 +7,7 @@ import qualified Text.Trifecta as Tri
 
 fromSuccess :: Tri.Result a -> a
 fromSuccess (Tri.Success a) = a
-fromSuccess _ = error "Failure"
+fromSuccess (Tri.Failure err) = error $ show err
 
 spec :: Spec
 spec = do
@@ -22,4 +22,10 @@ spec = do
       fromSuccess (parseComments "-- hoge\n-- piyo") `shouldBe` ["hoge", "piyo"]
     it "multiple block comments" $ do
       fromSuccess (parseComments "{- hoge\npiyo\nnyan -}") `shouldBe` ["hoge\npiyo\nnyan"]
+
+  describe "parseDoc" $ do
+    it "ascii section" $ do
+      fromSuccess (parseDoc "************************************************************************\n*                                                                      *\nLexical categories\n*                                                                      *\n************************************************************************") `shouldBe` Section "Lexical categories"
+    it "paragraph" $ do
+      fromSuccess (parseDoc "text text\ntext") `shouldBe` Paragraph "text text\ntext"
 
