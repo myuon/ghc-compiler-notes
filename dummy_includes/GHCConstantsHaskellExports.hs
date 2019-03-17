cc7006aa2cb2dc37b4085588cbdbe58a7dd4a218
Mizunashi Mana <mizunashi-mana@noreply.git>
2019-03-18 02:51:17 +0900

    To use pure GHC API

---
+++ dummy_includes/GHCConstantsHaskellExports.hs
@@ -0,0 +1,1 @@
+-- includes/dist-derivedconstants/header/GHCConstantsHaskellExports.hs
---
+++ src/GHC/Compiler/Notes/Types.hs
@@ -0,0 +1,38 @@
+module GHC.Compiler.Notes.Types where
+
+import           Data.Sequence ( Seq(..) )
+import           Data.Text     ( Text )
+import           SrcLoc
+
+
+newtype NoteId = NoteId Text
+  deriving (Eq, Show)
+
+data Note = Note
+  { noteId      :: NoteId
+  , noteContent :: Text
+  }
+  deriving (Eq, Show)
+
+
+data CollectedNotes = CollectedNotes
+  { notes    :: Seq (Located Note)
+  , noteRefs :: Seq (Located NoteId)
+  }
+
+instance Semigroup CollectedNotes where
+  ns1 <> ns2 = CollectedNotes
+    { notes    = notes ns1 <> notes ns2
+    , noteRefs = noteRefs ns1 <> noteRefs ns2
+    }
+
+instance Monoid CollectedNotes where
+  mempty = CollectedNotes
+    { notes    = mempty
+    , noteRefs = mempty
+    }
+
+addNoteByCollecting :: Located Note -> CollectedNotes -> CollectedNotes
+addNoteByCollecting n ns = ns
+  { notes = notes ns :|> n
+  }
